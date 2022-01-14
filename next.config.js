0b44720eca628bd8b6357360e4f07d18ee8742d4
Tibowl <Tibowl@users.noreply.github.com>
2022-01-14 21:08:41 +0100

    Add caching headers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,34 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["cdn.discordapp.com", "discord.com", "i.imgur.com"],
     minimumCacheTTL: 3600
   },
-  poweredByHeader: false
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/img/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=28800",
+          }
+        ],
+      },
+      {
+        source: "/favicon.ico",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400",
+          }
+        ],
+      },
+    ]
+  },
 }
