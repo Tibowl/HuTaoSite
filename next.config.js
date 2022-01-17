76c3b42489f71686e7077a72e652ff2952ed6fe3
Tibowl <Tibowl@users.noreply.github.com>
2022-01-17 14:49:25 +0100

    Increase cache time

--- next.config.js
+++ next.config.js
@@ -10,12 +10,12 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/img/:all*(svg|jpg|png)",
+        source: "/img/:all*(svg|jpg|png|gif|mp4|jpeg|webp)",
         locale: false,
         headers: [
           {
             key: "Cache-Control",
-            value: "public, max-age=28800",
+            value: "public, max-age=28800, s-max-age=604800",
           },
           {
             key: "Age",
